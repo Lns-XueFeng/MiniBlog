77550d4284c6fc1247270796a947988be6dd0c0d
handandkeep <1477792904@qq.com>
2024-02-01 16:36:17 +0800

    version-1.1

---
+++ miniblog/errors.py
@@ -0,0 +1,13 @@
+from flask import render_template
+
+from miniblog.app import app
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error/404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("error/500.html"), 500
